77c96aeb69031446768e2a0742be6847b581f8f4
Mathew Renjis <84729338+maxx41@users.noreply.github.com>
2022-06-28 16:53:09 +0530

    Update Velocitycmndsbased.py

--- Python_scripts/Velocitycmndsbased.py
+++ Python_scripts/Velocitycmndsbased.py
@@ -2,29 +2,27 @@ from __future__ import print_function
 import time
 from dronekit import connect, VehicleMode, LocationGlobalRelative
 from pymavlink import mavutil
+import time
+import socket
+import exceptions
 
 
 # Set up option parsing to get connection string
 import argparse
-parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
-parser.add_argument('--connect',
-                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+
+#Function to connect script to copter
+def connectMyCopter() :
+parser = argparse.ArgumentParser(description='Commands')
+parser.add_argument('--connect')
 args = parser.parse_args()
 
 connection_string = args.connect
-sitl = None
-
-
-# Start SITL if no connection string specified
-if not connection_string:
-    import dronekit_sitl
-    sitl = dronekit_sitl.start_default()
-    connection_string = sitl.connection_string()
-
-
-# Connect to the Vehicle
-print('Connecting to vehicle on: %s' % connection_string)
 vehicle = connect(connection_string, wait_ready=True)
+
+return vehicle
+
+
+#Definition to arm the drone and takeoff
 
 
 def arm_and_takeoff(aTargetAltitude):
@@ -40,14 +38,18 @@ def arm_and_takeoff(aTargetAltitude):
 
     print("Arming motors")
     # Copter should arm in GUIDED mode
-    vehicle.mode = VehicleMode("GUIDED")
+    while vehicle.mode! = VehicleMode("GUIDED"):
+	print("Waiting for vehicle to enter guided mode")
+	time.sleep(1)
+	
+    # Arming after confirming guided mode
     vehicle.armed = True
     vehicle.channels.overrides['3']= 1500
     # Confirm vehicle armed before attempting to take off
-    while not vehicle.armed:
+    while vehicle.armed==False:
         print(" Waiting for arming...")
         time.sleep(1)
-
+    
     print("Taking off!")
     vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
 
@@ -61,17 +63,18 @@ def arm_and_takeoff(aTargetAltitude):
 def set_velocity_body(Vx,Vy,Vz) :
 	
 	msg = vehicle.message_factory.net_position_target_local_ned_encode(
-	0,
-	0,0
-	mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
-	0b0000111111000111, #bitmask
-	0,0,0,#position
-	Vx,Vy,Vz,#velocity
-	0,0,0, #accelaration
-	0,0)
+		0,
+		0,0
+		mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
+		0b0000111111000111, #bitmask
+		0,0,0, #position
+		Vx,Vy,Vz, #velocity
+		0,0,0, #accelaration
+		0,0)
 	vehicle.send_mavlink(msg)
 	vehicle.flush()
 
+####################################
 
 vehicle=connectMyCopter()
 print("About to takeoff...")
@@ -79,7 +82,7 @@ print("About to takeoff...")
 arm_and_takeoff(2)
 
 c=0
-
+ 
 while c<2:
 	set_velocity_body(1,0,0)
 	print("Heads 1 unit north")
@@ -105,3 +108,4 @@ print("End of function")
 
 while True:
 	time.sleep(2)
+vehicle.close()
